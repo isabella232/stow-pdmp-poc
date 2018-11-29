66a0f94ad4edb899a8a4f4bfe72348985b032e75
Anuj <ashah812@gmail.com>
2018-11-29 15:06:19 -0800

    prescription page

--- components/Prescription.js
+++ components/Prescription.js
@@ -1,48 +1,75 @@
-import React from 'react';
-import theme from '@stowprotocol/brand/theme';
+import React from "react";
+import theme from "@stowprotocol/brand/theme";
 import { Col, Row, Grid } from "react-native-easy-grid";
-import { StyleSheet, Text, AsyncStorage } from 'react-native';
-import Button from './Button';
+import { StyleSheet, Text, AsyncStorage } from "react-native";
+import Button from "./Button";
 
 const goBack = navigation => async () => {
-   	navigation.goBack();
+  navigation.goBack();
 };
 
 const Prescription = ({ navigation }) => {
-	const prescription = navigation.getParam('prescription')
-	return (
-	<Grid style={styles.root}>
-		<Row style={styles.row}>
-			<Text style={styles.title}>
-				Prescription
-			</Text>
-		</Row>
-		<Row></Row>
-		<Row style={styles.row}>
-			<Button
-				title='Purge'
-				onPress={goBack(navigation)}
-			/>
-		</Row>
-	</Grid>
-)};
-
+  const prescription = navigation.getParam("prescription");
+  console.log(prescription);
+  return (
+    <Grid style={styles.root}>
+      <Row style={styles.row}>
+        <Text style={styles.title}>{prescription.metadata.substance}</Text>
+      </Row>
+      <Row style={styles.row}>
+        <Text style={styles.text}>
+          Prescribed: {new Date(prescription.metadata.date).toString()}
+        </Text>
+      </Row>
+      <Row style={styles.row}>
+        <Text style={styles.text}>Dosage:</Text>
+        <Text style={styles.subtext}> {prescription.metadata.dosage} </Text>
+      </Row>
+      <Row style={styles.row}>
+        <Text style={styles.text}>Frequency:</Text>
+        <Text style={styles.subtext}> {prescription.metadata.frequency} </Text>
+      </Row>
+      <Row style={styles.row}>
+        <Text style={styles.text}>Duration:</Text>
+        <Text style={styles.subtext}> {prescription.metadata.duration} </Text>
+      </Row>
+      <Row style={styles.row}>
+        <Text style={styles.text}>Refills:</Text>
+        <Text style={styles.subtext}> {prescription.metadata.refills} </Text>
+      </Row>
+      <Row style={styles.row}>
+        <Button title="Back" onPress={goBack(navigation)} />
+      </Row>
+    </Grid>
+  );
+};
 
 const styles = StyleSheet.create({
   root: {
-	padding: 20,
-	textAlign: 'center',
-    backgroundColor: theme.palette.primary.main,
+    padding: 20,
+    textAlign: "center",
+    backgroundColor: theme.palette.primary.main
   },
   row: {
-  	flex: 1,
-  	justifyContent: 'center',
-  	alignItems: 'center',
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
   },
   title: {
-  	fontFamily: theme.typography.secondary,
-  	fontSize: 32
+    fontFamily: theme.typography.secondary,
+    fontSize: 32,
+    color: "white"
   },
+  text: {
+    color: "white",
+    fontFamily: theme.typography.secondary,
+    fontSize: 20
+  },
+  subtext: {
+    color: "white",
+    fontFamily: theme.typography.secondary,
+    fontSize: 15
+  }
 });
 
 export default Prescription;
--- components/PrescriptionList.js
+++ components/PrescriptionList.js
@@ -4,14 +4,13 @@ import {
   Text,
   TextInput,
   View,
-  Button,
   Dimensions,
   Animated,
   Easing
 } from "react-native";
 import { Col, Row, Grid } from "react-native-easy-grid";
 import theme from "@stowprotocol/brand/theme";
-import BUtton from "./Button";
+import Button from "react-native-button";
 
 class PrescriptionList extends React.Component {
   constructor(props) {
@@ -25,28 +24,33 @@ class PrescriptionList extends React.Component {
 
   componentDidMount() {
     fetch(
-      `https://qastg.api.stow-protocol.com/records?owner=${'0xa82ae2ac8d0c0f5399a68c44492cbc03f49a903d'}`
+      `https://qastg.api.stow-protocol.com/records?owner=${"0xa82ae2ac8d0c0f5399a68c44492cbc03f49a903d"}`
     )
       .then(response => response.json())
       .then(prescriptions => {
         // can edit this mapping once we have data to play with
-        prescriptions = prescriptions.map(prescription =>{
-          prescription.metadata = JSON.parse(prescription.metadata)
-          prescription.metadata.substance = 'hugs';
+        prescriptions = prescriptions.map(prescription => {
+          prescription.metadata = JSON.parse(prescription.metadata);
+          prescription.metadata.substance = "hugs";
           prescription.metadata.date = "2014-11-03T19:38:34.203Z";
+          prescription.metadata.dosage = "30mg";
+          prescription.metadata.frequency = "once a day";
+          prescription.metadata.duration = "for three weeks";
+          prescription.metadata.refills = "1";
           return prescription;
-          })
-        console.log(prescriptions)
+        });
         this.setState({ prescriptions });
       });
   }
 
-  share(){}
+  share() {}
 
   prescriptionMap(role) {
     return this.state.prescriptions.map(prescription => {
       return (
-        <Row style={styles.row} key={prescription.dataHash} onPress={() => this.props.navigation.navigate("Prescription", {prescription})}>
+        <Row style={styles.row} key={prescription.dataHash} onPress={() =>
+              this.props.navigation.navigate("Prescription", { prescription })
+            }>
           <Col>
             <Text style={styles.text}>{prescription.metadata.substance}</Text>
             <Text style={styles.subtext}>
@@ -54,10 +58,12 @@ class PrescriptionList extends React.Component {
             </Text>
           </Col>
           <Col>
-          {
-            // share function needs to be done Fill here is for pharmacy
-          }
-            <Button title={role === "patient" ? "Share" : "Fill"} />
+            {
+              // share function needs to be done Fill here is for pharmacy
+            }
+            <Button style={styles.button} onPress={this.share}>
+              {role === "patient" ? "Share" : "Fill"}
+            </Button>
           </Col>
         </Row>
       );
@@ -103,6 +109,16 @@ const styles = StyleSheet.create({
     fontFamily: theme.typography.secondary,
     textAlign: "center",
     fontSize: 32
+  },
+  button: {
+    color: "white",
+    backgroundColor: theme.palette.secondary.main,
+    fontFamily: theme.typography.secondary,
+    fontSize: 10,
+    paddingTop: 12,
+    paddingBottom: 12,
+    width: width - 230,
+    marginLeft: 20
   }
 });
 
